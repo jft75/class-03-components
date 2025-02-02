0d3ef900209514c4e4a857d5cca10b365a76c91c
jft75 <143815415+jft75@users.noreply.github.com>
2025-02-02 00:32:27 +0000

    completed class3

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,13 @@
 import './Home.css'
-import galaxyImage from '/images/galaxy.webp'
+import GalleryCard from './components/GalleryCard'
 
 export default function Home() {
   return (
     <main>
       <div className="gallery">
-        <div className="card">
-          <img className="image" src={galaxyImage} alt="an AI generated galaxy" />
-
-          <p className="description">
-            Source: <cite>Microsoft Copilot</cite>
-          </p>
-        </div>
+        <GalleryCard/>
       </div>
-
+      
       <h2>Rendered Date</h2>
       <p>{new Date().toLocaleString()}</p>
     </main>
